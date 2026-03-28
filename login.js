3f7cb45515376b81c11e74d9c556b7b40eb0454a
Samir kafle <sharbankafle01@gmail.com>
2026-03-28 21:12:38 +0545

    Added some js code in the login page

---
+++ login.js
@@ -0,0 +1,5 @@
+const logoContainer=document.querySelector(".left-container");
+//This code make open the index.html home file when click in the logo container of the login page
+logoContainer.addEventListener("click",()=>{
+    window.location.href = "index.html";
+});
