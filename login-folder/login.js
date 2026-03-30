3aee31d89d15adfbe98e81552ef19919e7f96193
Samir kafle <sharbankafle01@gmail.com>
2026-03-30 15:37:32 +0545

    added some folder and the files

--- login-folder/login.js
+++ login-folder/login.js
@@ -1,7 +1,7 @@
 const logoContainer=document.querySelector(".left-container");
 //This code make open the index.html home file when click in the logo container of the login page
 logoContainer.addEventListener("click",()=>{
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
 });
 const googleLink=document.querySelector(".google-container");
 const appleLink=document.querySelector(".apple-container");
